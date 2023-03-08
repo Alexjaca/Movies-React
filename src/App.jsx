dacd56f4ddc1f97afb3b907cd64ff6e0e71057c1
Alexjaca <jalexcardenasa@gmail.com>
2023-03-08 23:52:26 +0100

    prueba

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import styles from "./App.module.css";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import MovieDetails from "./pages/MovieDetails";
+import tachira from "./assets/images/tachira.png";
 
 export default function App() {
   return (
     <Router>
       <header>
         <Link to="/">
-          <h1 className={styles.title}>JAC Movies</h1>
+          <div className={styles.container}>
+          <h1 className={styles.title}>JAC Movies <img src={tachira} alt="tachira" className={styles.tachira}/></h1>
+          
+          </div>
         </Link>
       </header>
       <main>
